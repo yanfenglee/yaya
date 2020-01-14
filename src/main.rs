57e39f7b2e9d51f09ad0b3c571427093d05869a4
yanfeng.li <yanfeng.li@yunqueyi.com>
2020-01-14 21:20:00 +0800

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use bytes::BytesMut;
 use futures::SinkExt;
 use std::sync::mpsc::{Sender,Receiver,channel};
 use http::{Request, Response, StatusCode};
@@ -7,6 +6,8 @@ use tokio::net::TcpStream;
 use tokio::stream::StreamExt;
 use tokio_util::codec::{Decoder, Encoder, Framed};
 use std::time::{Instant};
+mod simple_http;
+use simple_http::Http;
 
 extern crate clap;
 use clap::{Arg, App};
@@ -111,82 +112,4 @@ async fn process(stream: TcpStream, tx: Sender<u8>) -> Result<(), Box<dyn Error>
     }
 
     Ok(())
-}
-
-
-struct Http;
-
-impl Encoder for Http {
-    type Item = Request<String>;
-    type Error = io::Error;
-
-    fn encode(&mut self, item: Request<String>, dst: &mut BytesMut) -> io::Result<()> {
-        use std::fmt::Write;
-
-        write!(
-            BytesWrite(dst),
-            "\
-            GET {} HTTP/1.1\r\n\
-            Content-Length: {}\r\n\
-            ",
-            item.uri().path(),
-            item.body().len(),
-        )
-        .unwrap();
-
-        for (k, v) in item.headers() {
-            dst.extend_from_slice(k.as_str().as_bytes());
-            dst.extend_from_slice(b": ");
-            dst.extend_from_slice(v.as_bytes());
-            dst.extend_from_slice(b"\r\n");
-        }
-
-        dst.extend_from_slice(b"\r\n");
-        dst.extend_from_slice(item.body().as_bytes());
-
-        return Ok(());
-
-        // Right now `write!` on `Vec<u8>` goes through io::Write and is not
-        // super speedy, so inline a less-crufty implementation here which
-        // doesn't go through io::Error.
-        struct BytesWrite<'a>(&'a mut BytesMut);
-
-        impl fmt::Write for BytesWrite<'_> {
-            fn write_str(&mut self, s: &str) -> fmt::Result {
-                self.0.extend_from_slice(s.as_bytes());
-                Ok(())
-            }
-
-            fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> fmt::Result {
-                fmt::write(self, args)
-            }
-        }
-    }
-}
-
-impl Decoder for Http {
-    type Item = Response<()>;
-    type Error = io::Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> io::Result<Option<Self::Item>> {
-        
-        //println!("decoding: {:?}", src);
-
-        let mut parsed_headers = [httparse::EMPTY_HEADER; 16];
-        let mut r = httparse::Response::new(&mut parsed_headers);
-        let _status = r.parse(src).map_err(|e| {
-            let msg = format!("failed to parse http response: {:?}", e);
-            io::Error::new(io::ErrorKind::Other, msg)
-        })?;
-
-        let code = match r.code {
-            Some(200) => StatusCode::OK,
-            _ => StatusCode::INTERNAL_SERVER_ERROR
-        };
-
-        let resp = Response::builder().status(code).body(()).unwrap();
-
-        Ok(Some(resp))
-
-    }
 }
---
+++ src/simple_http.rs
@@ -0,0 +1,83 @@
+use bytes::BytesMut;
+use http::{Request, Response, StatusCode};
+use std::{error::Error, fmt, io};
+use tokio::net::TcpStream;
+use tokio::stream::StreamExt;
+use tokio_util::codec::{Decoder, Encoder, Framed};
+
+pub struct Http;
+
+impl Encoder for Http {
+    type Item = Request<String>;
+    type Error = io::Error;
+
+    fn encode(&mut self, item: Request<String>, dst: &mut BytesMut) -> io::Result<()> {
+        use std::fmt::Write;
+
+        write!(
+            BytesWrite(dst),
+            "\
+            GET {} HTTP/1.1\r\n\
+            Content-Length: {}\r\n\
+            ",
+            item.uri().path(),
+            item.body().len(),
+        )
+        .unwrap();
+
+        for (k, v) in item.headers() {
+            dst.extend_from_slice(k.as_str().as_bytes());
+            dst.extend_from_slice(b": ");
+            dst.extend_from_slice(v.as_bytes());
+            dst.extend_from_slice(b"\r\n");
+        }
+
+        dst.extend_from_slice(b"\r\n");
+        dst.extend_from_slice(item.body().as_bytes());
+
+        return Ok(());
+
+        // Right now `write!` on `Vec<u8>` goes through io::Write and is not
+        // super speedy, so inline a less-crufty implementation here which
+        // doesn't go through io::Error.
+        struct BytesWrite<'a>(&'a mut BytesMut);
+
+        impl fmt::Write for BytesWrite<'_> {
+            fn write_str(&mut self, s: &str) -> fmt::Result {
+                self.0.extend_from_slice(s.as_bytes());
+                Ok(())
+            }
+
+            fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> fmt::Result {
+                fmt::write(self, args)
+            }
+        }
+    }
+}
+
+impl Decoder for Http {
+    type Item = Response<()>;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> io::Result<Option<Self::Item>> {
+        
+        //println!("decoding: {:?}", src);
+
+        let mut parsed_headers = [httparse::EMPTY_HEADER; 16];
+        let mut r = httparse::Response::new(&mut parsed_headers);
+        let _status = r.parse(src).map_err(|e| {
+            let msg = format!("failed to parse http response: {:?}", e);
+            io::Error::new(io::ErrorKind::Other, msg)
+        })?;
+
+        let code = match r.code {
+            Some(200) => StatusCode::OK,
+            _ => StatusCode::INTERNAL_SERVER_ERROR
+        };
+
+        let resp = Response::builder().status(code).body(()).unwrap();
+
+        Ok(Some(resp))
+
+    }
+}
